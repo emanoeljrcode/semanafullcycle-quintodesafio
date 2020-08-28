4b3990e3f9eb5f072b709aed2d8572d46d9a5c20
Emanoel <emchavesjr@gmail.com>
2020-08-28 14:34:00 -0300

    Testes editados

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Eu serei um FullCycle"', () => {
+      expect(appController.getHello()).toBe('Eu serei um FullCycle');
     });
   });
 });
